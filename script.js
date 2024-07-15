1f0777d99be1d857afd3ea772a0c163270b216eb
TungSon <t.sonn312001@gmail.com>
2024-07-16 00:29:47 +0700

    AddFeature | #4.1 Completed AddFeature

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
+const taskList = document.querySelector('.list__task');
 function renderTasks() {
-    const taskList = document.getElementById('task-list');
+    
     taskList.innerHTML = '';
     tasks.forEach((task, index) => {
         const taskItem = document.createElement('li');
         taskItem.className = task.isDone ? 'done' : '';
         taskItem.innerHTML = `
-            <span>${task.name}</span>
-            <div>
-                <input type="checkbox" ${task.isDone ? 'checked' : ''} onclick="toggleTask(${index})">
+
+                <input class="check-box" type="checkbox" ${task.isDone ? 'checked' : ''} onclick="toggleTask(${index})">
+                <span>${task.name}</span>
                 <button onclick="editTask(${index})">Edit</button>
                 <button onclick="deleteTask(${index})">Delete</button>
-            </div>
+
         `;
         taskList.appendChild(taskItem);
     });
 }
+
+let tasks = [];
+
+const addButton = document.querySelector('.add__button');
+
+addButton.onclick = () => {
+    const taskNameInput = document.querySelector('.add__input');
+    const taskName = taskNameInput.value;
+
+    if (taskName) {
+        tasks.push({ name: taskName, isDone: false });
+        taskNameInput.value = '';
+        renderTasks();
+    }
+}
+const cancelButton = document.querySelector('.cancel__button');
+cancelButton.onclick = () => {
+
+    document.querySelector('.add__input').value = '';
+}
+// function clearTask() {
+//     const addButton = document.querySelector('.add__button');
+//     document.getElementById('.list__task').value = '';
+// }
