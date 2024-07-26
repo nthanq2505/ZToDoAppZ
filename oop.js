6d791f5500874de0f713b56b70e78f512b3b6be3
nthanq2505 <20127627@student.hcmus.edu.vn>
2024-07-26 18:25:16 +0700

    AddFeatureOOP | #9.1 Complete add feature OOP

--- oop.js
+++ oop.js
@@ -1,1 +1,58 @@
 // This file to change the object oriented programming in javascript
+function TaskManager() {
+    this.tasks = [];
+    this.currentTaskIndex = null;
+    this.currentFilter = 'all';
+
+    this.taskList = document.querySelector('.list__task');
+    this.addButton = document.querySelector('.add__button');
+    this.cancelButton = document.querySelector('.cancel__button');
+
+    this.addButton.onclick = this.addTask.bind(this);
+    this.cancelButton.onclick = this.cancelAdd.bind(this);
+
+    this.renderTasks();
+}
+
+TaskManager.prototype.addTask = function() {
+    const taskNameInput = document.querySelector('.add__input');
+    const taskName = taskNameInput.value;
+
+    if (taskName) {
+        this.tasks.push({ name: taskName, isDone: false });
+        taskNameInput.value = '';
+        this.renderTasks();
+    }
+};
+
+TaskManager.prototype.cancelAdd = function() {
+    document.querySelector('.add__input').value = '';
+};
+
+TaskManager.prototype.toggleTask = function(index) {
+    this.tasks[index].isDone = !this.tasks[index].isDone;
+    this.renderTasks();
+};
+
+TaskManager.prototype.renderTasks = function() {
+    this.taskList.innerHTML = '';
+
+
+    this.tasks.forEach((task, index) => {
+        const taskItem = document.createElement('li');
+        taskItem.innerHTML = `
+            <input class="check-box" type="checkbox" ${task.isDone ? 'checked' : ''} onclick="taskManager.toggleTask(${index})">
+            <span>${task.name}</span>
+            <button onclick="taskManager.editTask(${index})">Edit</button>
+            <button class="red-button" onclick="taskManager.deleteTask(${index})">Delete</button>
+        `;
+        this.taskList.appendChild(taskItem);
+    });
+};
+
+const taskManager = new TaskManager();
+
+
+
+
+
