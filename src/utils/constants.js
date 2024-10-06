9e2a75d017116047278a01af785df62f0020fa1e
CuongPC <manhcuongno3@gmail.com>
2024-10-06 23:21:04 +0700

    Delete extra code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,5 @@
 export const API_ROOT = 'http://127.0.0.1:8000'
 
-export const httpStatusCodes = {
-  OK: 200,
-  CREATED: 201,
-  NO_CONTENT: 204,
-  BAD_REQUEST: 400,
-  UNAUTHORIZED: 401,
-  NOT_FOUND: 404,
-  CONFLICT: 409,
-  INTERNAL_SERVER_ERROR: 500
-}
-
 export const StateOfFilterTasks = Object.freeze({
   ALL: "all",
   DONE: "done",
